fb31c8b85bcdfce82c945b2ad211c2701c4f3ac5
ZiadAbass <el16za@leeds.ac.uk>
2021-01-14 17:44:52 +0200

    Added function to extract a single body segment from the data

---
+++ extract_segment.m
@@ -0,0 +1,43 @@
+%{
+This function takes in the name of the most significant body segment, as
+well as the dataset.
+It extracts all of the segment's information from the data, and returns the
+dataset after reducing it to only data from that segment.
+%}
+
+function [segment_features_labelled_data, segment_features_inputs_svm]=extract_segment(keyword, final_labelled_data, final_inputs_svm)
+
+    % define the body part we are looking for
+
+    % labels.csv contains all the class labels in english in the same order 
+    % that they appear in the data
+    feature_labels = readtable("labels.csv", "ReadVariableNames",true, 'Delimiter','comma');
+    ll = 0;
+    % sig_indexes will contain the indexes of the features
+    % related to the relevant body part i.e. keyword given.
+    sig_indexes = zeros(1,42);
+    feature_labels_array = table2array(feature_labels);
+
+    % Loop through the feature names
+    for ii=1 : size(feature_labels_array,1)
+        % Find features that have the word we are looking for
+        is_present = contains(feature_labels_array(ii),keyword,'IgnoreCase',true);
+        if is_present
+            % Its index correlates to the same index in the data as class labels are in
+            % the same order
+            sig_indexes(1,ll+1) = ii;
+            ll = ll+1;
+        end
+
+    end
+
+    % extract just the segment's columns from the data (NN data)
+    class_labels = final_labelled_data(:,end-4:end);
+    segment_features_unlabelled = final_labelled_data(:,sig_indexes);
+    segment_features_labelled_data = horzcat(segment_features_unlabelled, class_labels);
+
+    % extract just the segment's columns from the data (SVM data)
+    segment_features_inputs_svm = final_inputs_svm(:,sig_indexes);
+end
+
+
--- main.m
+++ main.m
@@ -1,6 +1,11 @@
 clear
 clc
 
+
+% ======================================
+% ================ TASK 1 ==============
+% ======================================
+
 fprintf("\n--------\n Importing the data...\n--------\n")
 new_import_datasets;
 
@@ -9,7 +14,6 @@ filter_data;
 
 fprintf("\n--------\n Extracting the time-domain features...\n--------\n")
 extract_reduce_features;
-% delete all the vars except the ones we need
 clearvars -except raw_data processed_data
 
 fprintf("\n--------\n Organising the features...\n--------\n")
@@ -29,19 +33,47 @@ close all
 fprintf("\n--------\n Training the SVM using all the features...\n--------\n")
 svm_posterior(final_inputs_svm, final_targets_svm)
 
+
+% ======================================
+% ================ TASK 2 ==============
+% ======================================
+
 fprintf("\n--------\n Finding the top 15 features...\n--------\n")
 find_15_features
 clearvars -except sorted_labels sorted_labels_cell fifteen_features_inputs_svm fifteen_features_inputs_nn fifteen_features_labelled_data final_targets_nn final_targets_svm
 
 fprintf("\n--------\n Training the ANN using 15 features...\n--------\n")
 nn_manual(fifteen_features_labelled_data)
-clearvars -except sorted_labels sorted_labels_cell fifteen_features_inputs_svm final_targets_svm
+clearvars -except sorted_labels sorted_labels_cell fifteen_features_inputs_svm final_targets_svm final_labelled_data final_inputs_svm
 
 fprintf("\n--------\n Training the SVM using 15 features...\n--------\n")
 svm_posterior(fifteen_features_inputs_svm, final_targets_svm)
 
+% ======================================
+% ================ TASK 3 ==============
+% ======================================
+
+fprintf("\n--------\n Finding features from a single segment...\n--------\n")
+[segment_features_labelled_data, segment_features_inputs_svm] = extract_segment("Thigh", final_labelled_data, final_inputs_svm);
+
+% ======================================
+% ================ TASK 4 ==============
+% ======================================
+
+fprintf("\n--------\n Training the ANN using features from a single segment...\n--------\n")
+nn_manual(segment_features_labelled_data)
+
+fprintf("\n--------\n Training the SVM using features from a single segment...\n--------\n")
+svm_posterior(segment_features_inputs_svm, final_targets_svm)
+
 
 %{
 TODO:
-- Compare the networks with all features vs with only 15 features
+- Compare the networks with all features vs with only 15 features vs with a
+single segment?
 %}
+
+
+
+
+
